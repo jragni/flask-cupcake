604db0a2ea668ea629eeba13f32e926609d299f3
jragni <jagni001@ucr.edu>
2021-07-19 17:59:34 -0700

    FEAT: got the MVP completed

--- static/cupcakes.js
+++ static/cupcakes.js
@@ -1,7 +1,7 @@
 "use strict"
 const BASE_API_URL = 'http://localhost:5000/api/';
 const $cupcakeList = $('#cupcake-list');
-
+const $submitBtn = $('#submit-btn')
 /** getCupcakes
  * Description: function that requests cupcake data from API and displays it on the DOM
  */
@@ -19,6 +19,24 @@ async function listCupcakes(){
         $cupcakeList.append( $li.text(cupcake.flavor));
     })
 }
+
+$submitBtn.on('click', submitCallback)
+
+async function submitCallback(evt) {
+    console.log('we made it -- submit')
+    evt.preventDefault();
+    // get form values
+    let flavor = $("#flavor").val();
+    let size = $("#size").val();
+    let rating = $("#rating").val();
+    let image = $("#image").val();
+    let data = {flavor, size, rating, image};
+    let response = await axios.post(`${BASE_API_URL}cupcakes`, data);
+    console.log(response);
+}
+
+
+// TODO: append dom with new 
 
 /** main
  * Description: runs on start
